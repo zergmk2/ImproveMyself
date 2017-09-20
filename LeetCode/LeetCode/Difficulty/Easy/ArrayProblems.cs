90420fab929562a00d79facdfcad9c3b2bcece52
zergmk2 <zergmk2@gmail.com>
2017-09-21 00:08:15 +0800

    27. Remove Element

---
+++ LeetCode/LeetCode/Difficulty/Easy/ArrayProblems.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Difficulty.Easy
+{
+    public class ArrayProblems
+    {
+        public static int RemoveElement(int[] nums, int val)
+        {
+            if (nums == null)
+            {
+                return 0;
+            }
+            else if (nums.Length == 0)
+            {
+                return 0;
+            }
+
+            int head = 0, tail = nums.Length - 1;
+            while(head <= tail)
+            {
+                if (tail == head)
+                {
+                    if (nums[head] == val)
+                    {
+                        head--;
+                    }
+                    break;
+                }
+
+                if (nums[head] == val)
+                {
+                    while (tail > head && nums[tail] == val)
+                    {
+                        tail--;
+                    }
+
+                    if (tail == head)
+                    {
+                        head--;
+                        break;
+                    }
+                    else
+                    {
+                        nums[head] = nums[tail];
+                        tail--;
+                        if (head == tail)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            head++;
+                        }
+                    }
+                }
+                else
+                {
+                    head++;
+                }
+            }
+
+            return head + 1;
+        }
+    }
+}
---
+++ LeetCode/UnitTest/Difficulty/Easy/ArrayProblemsTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Difficulty.Easy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Difficulty.Easy.Tests
+{
+    [TestClass()]
+    public class ArrayProblemsTests
+    {
+        [TestMethod()]
+        public void RemoveElementTest()
+        {
+            int[] nums = { 3, 2, 2, 3 };
+            int cnt = ArrayProblems.RemoveElement(nums, 3);
+            Assert.IsTrue(cnt == 2);
+        }
+    }
+}
